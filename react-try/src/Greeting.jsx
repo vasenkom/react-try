361b24bf32815238be61683d34f9d448373aecdd
vasenkom <m.vasenko@yandex.ru>
2024-08-11 16:46:05 +0200

    create Greet function and import it to the main.jsx

---
+++ react-try/src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>&quot;Hello! I will show you my favourite food!&quot;</h1>;
+}
+
+export default Greeting;
--- react-try/src/main.jsx
+++ react-try/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+
+import Greeting from "./Greeting.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <h1>hello world</h1>
+    <Greeting />
   </StrictMode>
 );

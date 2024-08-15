a5f7971dc065f21f42370265d8bc26d9cc28df58
vasenkom <m.vasenko@yandex.ru>
2024-08-15 21:39:52 +0200

    create food related function

---
+++ react-try/src/Food.jsx
@@ -0,0 +1,12 @@
+function AvocadoToast() {
+  return (
+    <div>
+      <img
+        src="https://mytasteforliving.com/img/85/title.JPG"
+        style={{ width: "300px", height: "200px" }}
+      ></img>
+      <p>Avocado Toast</p>
+    </div>
+  );
+}
+export { AvocadoToast };

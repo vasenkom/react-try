a428b880cebbf4acaf00f762a413fe4d105a6915
vasenkom <m.vasenko@yandex.ru>
2024-08-16 21:54:40 +0200

    update main

--- react-try/src/main.jsx
+++ react-try/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AvocadoToast } from "./Food.jsx";
 import Greeting from "./Greeting.jsx";
+import App2 from "./props2.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Greeting />
     <AvocadoToast />
+    <App2 />
   </StrictMode>
 );
